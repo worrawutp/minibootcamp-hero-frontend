fd687a7eae848a9135885a6f6d65fe5ee4fb17e8
zx4545zx <70090705+zx4545zx@users.noreply.github.com>
2021-07-13 21:13:26 +0700

    able to create hero with new api url

--- index.js
+++ index.js
@@ -3,9 +3,11 @@ import './src/main.scss'
 
 document.addEventListener('DOMContentLoaded', function() {
   let listHeroesDom = document.getElementById('list-heroes')
-  if(listHeroesDom == null) { return }
+  let formHero = document.querySelector("form")
 
+  if(listHeroesDom == null) { return }
   let url = process.env.API_URL + "/heroes"
+  formHero.setAttribute("action",url);
   fetch(url, {
     method: "GET",
     headers: {
@@ -26,12 +28,15 @@ document.addEventListener('DOMContentLoaded', function() {
     }
   }).then(resp => resp.json())
     .then(data => {
+      
       let jobWrapper = document.getElementById('job-wrapper')
       if(jobWrapper == null) { return }
 
       console.log(data)
       buildJobDropdown(jobWrapper, data)
     })
+
+  
 })
 
 function buildJobDropdown(targetDom, data) {
@@ -59,7 +64,8 @@ function buildHeroDom(targetDom, data) {
       <div class="hero">
         <a href="" class="hero-name">${hero.name}</a>
         <div>${hero.job}</div>
-        <div>${hero.hp}<div>
+        <div>${hero.hp}</div>
+        <div>${hero.mp}</div>
       </div>
     `
     targetDom.insertAdjacentHTML('beforeend', htmlStr) 
